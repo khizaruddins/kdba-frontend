5b7a2a8ba6dccaf6f3b5f1377be46212870bab68
Syed Khizaruddin <khizaruddins@gmail.com>
2026-08-23 20:38:28 +0530

    feat: env based api calls

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,6 +1,10 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api/v1';
+const API_URL =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  process.env.BASE_URL ||
+  process.env.NEXT_PUBLIC_API_URL ||
+  'http://localhost:4000/api/v1';
 
 export const apiClient = axios.create({
   baseURL: API_URL,
